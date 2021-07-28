96da4772a3bdfb67b99cb32778476bb15f6ddb45
donkatz <82435693+donkatz@users.noreply.github.com>
2021-07-28 22:56:01 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,28 @@
-$(Document).ready(function(){
-    
+$(Document).ready(function () {
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+    }
+  ]
+    });
+
 });
