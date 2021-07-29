271f5658ef1dd70e176c32b0bd3146867c280368
donkatz <82435693+donkatz@users.noreply.github.com>
2021-07-29 18:13:28 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,28 +1,6 @@
-$(Document).ready(function () {
-    $('.center').slick({
-        centerMode: true,
-        centerPadding: '60px',
+$(document).ready(function () {
+    $('.culture-slide').slick({
         slidesToShow: 3,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-    },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-    }
-  ]
     });
 
 });
