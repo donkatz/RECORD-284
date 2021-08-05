bedbb84d5ac6c2821c945eb128fa33b906d2963c
donkatz <82435693+donkatz@users.noreply.github.com>
2021-08-05 18:14:40 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,8 +1,28 @@
-$(document).ready(function () {
-    $('.culture-slide').slick({
-        slidesToShow: 3,
-        centerMode : true,
-        centerPadding : "0px"
-    });
+$(document).ready(function(){
+  
+    $(".culture-slide").slick({
+      slidesToShow: 3 ,
+      centerMode: true ,
+      centerPadding: "0px" ,
+      arrows: true,
 
-});
+      prevArrow: "<div class='before-btn'><img src='img/left-arrow.png' alt=''></div>", 
+      
+      nextArrow: "<div class='next-btn'><img src='img/next-arrow.png' alt=''></div>", 
+    });
+    
+    $('.culture-slide').on('init', function(event, slick){
+        $(".slick-center").addClass("active");
+    });
+    
+    $('.culture-slide').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      console.log("넘어가기 시작");
+      $(".slick-center").removeClass("active");
+    });
+    $('.culture-slide').on('afterChange', function(event, slick, currentSlide){
+      console.log("넘어가기 끝");
+      $(".slick-center").addClass("active");
+    });
+    
+
+  });
